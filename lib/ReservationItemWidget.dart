03fc9433d34b2d1594da3958f9433c56f09253fc
ChiranthAP <chiranthap27@gmail.com>
2024-10-15 21:40:39 +0530

    homepage ongoing_changes

--- lib/ReservationItemWidget.dart
+++ lib/ReservationItemWidget.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'ReservationDialogBox.dart';
+
 class ReservationItemWidget extends StatelessWidget {
   final String title;
   final String reservationNumber;
@@ -7,6 +9,14 @@ class ReservationItemWidget extends StatelessWidget {
   final String quantity;
   final Color titleColor;
   final Color backgroundColor;
+  final DateTime reservationDate;
+  final String machineName;
+  final String period;
+  final String unitPrice;
+  final int numberOfDays;
+  final String amount;
+  final String consumptionTax; // Add consumption tax parameter
+  final String total; // Add total parameter
 
   const ReservationItemWidget({
     Key? key,
@@ -16,6 +26,14 @@ class ReservationItemWidget extends StatelessWidget {
     required this.quantity,
     required this.titleColor,
     required this.backgroundColor,
+    required this.reservationDate,
+    required this.machineName,
+    required this.period,
+    required this.unitPrice,
+    required this.numberOfDays,
+    required this.amount,
+    required this.consumptionTax, // Initialize consumption tax
+    required this.total, // Initialize total
   }) : super(key: key);
 
   @override
@@ -36,24 +54,38 @@ class ReservationItemWidget extends StatelessWidget {
           ),
           const SizedBox(height: 8.0),
           Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               _buildColumn('予約No.', reservationNumber),
-              const Spacer(), // Equal spacing
               _buildColumn('利用期間', _formatUsagePeriod(usagePeriod)),
-              const Spacer(), // Equal spacing
               _buildColumn('数量', quantity),
-              const Spacer(), // Equal spacing
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  _buildColumn('詳細', ''), // Detail header
-                  IconButton(
-                    icon: const Icon(Icons.more_horiz), // Ellipsis button
-                    onPressed: () {
-                      // Handle button press here
+              _buildColumn('詳細', ''), // Detail header
+            ],
+          ),
+          const SizedBox(height: 8.0), // Space below headers
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              IconButton(
+                icon: const Icon(Icons.more_horiz), // Ellipsis button
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return ReservationDialogBox(
+                        title: title,
+                        reservationNumber: reservationNumber,
+                        usagePeriod: usagePeriod,
+                        quantity: quantity,
+                        amount: amount,
+                        consumptionTax: consumptionTax,
+                        backgroundColor: backgroundColor, machineName: machineName,
+                        numberOfDays:numberOfDays,period: period,
+                        reservationDate: reservationDate,total: total,unitPrice: unitPrice,
+                      );
                     },
-                  ),
-                ],
+                  );
+                },
               ),
             ],
           ),
@@ -67,6 +99,7 @@ class ReservationItemWidget extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Text(title, style: const TextStyle(fontWeight: FontWeight.bold)),
+        const SizedBox(height: 4.0), // Padding between header and data
         Text(value),
       ],
     );
